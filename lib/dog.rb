b87bc192cb01dfee06e0faca722f1e717d961621
Julio Hernandez <julioandres72@gmail.com>
2020-11-16 17:30:43 -0500

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,40 @@
 class Dog
+
     def bark
         puts "Woof!"
-    def sit
-        puts "The Dog is sitting"    
     end
-end
+
+    def sit
+        puts "The Dog is sitting"
+    end
+
+
+
 end
 
-class Person
-    def talk
-        puts "Hello World!"
-    def walk
-        puts "The Person is walking"
-    end
-end
-end
+
+
+
+
+
+
+
+
+
+# class Dog
+#     def bark
+#         puts "Woof!"
+#     def sit
+#         puts "The Dog is sitting"    
+#     end
+# end
+# end
+
+# class Person
+#     def talk
+#         puts "Hello World!"
+#     def walk
+#         puts "The Person is walking"
+#     end
+# end
+# end
